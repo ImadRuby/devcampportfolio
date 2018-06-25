d9b5383b194da26b207610bb021767d9ff912581
ImadRuby <imad.kadaikar@gmail.com>
2018-06-25 09:24:57 +0200

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+	#givme all path for portfolios, except for the method "show" because i want a different route.
+  resources :portfolios, except: [:show]
+  	#this is the nroute i want (portfolio in singular)
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs
-  resources :portfolios
+  
 
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
+  
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
